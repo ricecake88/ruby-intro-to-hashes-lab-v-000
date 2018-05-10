2dbc62829e7b712781e722ee124ceb9510bc0bd8
Grace Shih <vocalise@gmail.com>
2018-05-10 00:18:29 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,8 @@ def monopoly_with_third_tier
 	      },
 	      :b_and_o_railroad => {
 	        
-	      }
+	      },
+	      :shortline => {}
 	    },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
